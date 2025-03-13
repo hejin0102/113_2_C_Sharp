da4ecde768c7c0447d15fc80d6088c874dc95837
hejin0102 <a113221047@mail.shu.edu.tw>
2025-03-13 13:00:56 +0800

    2025_03_13

--- 114_03_06/Program5_13/Program5_13/Form1.cs
+++ 114_03_06/Program5_13/Program5_13/Form1.cs
@@ -19,34 +19,7 @@ namespace Program5_13
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Random random = new Random();
-            StreamWriter outputFile; //宣告StreamWriter物件
-            int count; //宣告整數變數
-
-            try
-            {
-                outputFile = File.AppendText("numbers.txt"); //開啟檔案，寫入數字
-                if (int.TryParse(textBox1.Text, out count))
-                {
-                    for (int i = 0; i < count; i++)
-                    {
-                        outputFile.WriteLine(random.Next(100) + 1); //寫入亂數
-                    }
-                    outputFile.Close(); //關閉檔案
-                    MessageBox.Show("檔案已經建立"); //顯示訊息
-                }
-                else
-                {
-                    MessageBox.Show("請輸入數字"); //顯示訊息
-                }
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message); //顯示錯誤訊息
-            }
-
-
+            
         }
     }
 }
